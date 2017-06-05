81941b87fcd937f3bf89f92da98d36b61abc183e
Noraw <maabernethy@wifi-172-23-95-160.uoa-wifi.auckland.ac.nz>
2017-06-06 00:29:01 +1200

    fix semicolon line number

--- app/components/error-bodies/expected-semicolon-error.js
+++ app/components/error-bodies/expected-semicolon-error.js
@@ -13,16 +13,12 @@ export default Ember.Component.extend({
     this.testUpdatedCode();
   },
 
-  lineNumber: computed('error.line', function() {
-    return get(this, 'error.line') - 1;
-  }),
-
   testUpdatedCode() {
     let source = get(this, 'source');
     let lineNumber = get(this, 'error.line');
     let sourceArray = source.split("\n");
-    let updatedLine = `${sourceArray[lineNumber-2]};`;
-    sourceArray[lineNumber-2] = updatedLine;
+    let updatedLine = `${sourceArray[lineNumber-1]};`;
+    sourceArray[lineNumber-1] = updatedLine;
     let updatedSource = sourceArray.join("\n");
 
     this.get('ajax').compilerRequest(updatedSource)
@@ -35,7 +31,7 @@ export default Ember.Component.extend({
 
   processReturnResult(response, updatedLine) {
     let { result: result, errorMessage: errorMessage, stdout: stdout, stderr: stderr } = this.get('ajax').processOutcome(response);
-    if (this.get('ajax').noCompileErrors(errorMessage)) {
+    if (this.get('ajax').lessNumberOfErrors(errorMessage)) {
       set(this, 'showCorrectCode', true);
       set(this, 'correctCode', updatedLine);
     }
